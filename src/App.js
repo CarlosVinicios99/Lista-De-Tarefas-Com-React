313e2168f94e86fbe25877cb1e89e5984619021a
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-02-18 22:12:12 -0300

    formulario implementado

--- src/App.js
+++ src/App.js
@@ -11,16 +11,50 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+
+    setTitle("")
+    setTime("")
+  }
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>Tarefas</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <p>Insira sua próxima tarefa</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">Qual sua tarefa?</label>
+            <input type="text" name="title" placeholder="Título da tarefa" 
+              onChange={(e) => {setTitle(e.target.value)}}
+              value={title || ""}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração: </label>
+            <input type="text" name="time" placeholder="Tempo estimado (em horas)" 
+              onChange={(e) => {setTime(e.target.value)}}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa"/>
+        </form>
       </div>
       <div className="list-todo">
-        <p>Lista</p>
+        <h2>Lista De Tarefas:</h2>
+        {todos.length === 0 && <p>Nao há tarefas!</p>} 
       </div>
     </div>
   )
